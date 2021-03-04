43abbb37237885491e8b47665aa6f57349539e0a
Mohammed Azad <mmazad@ualberta.ca>
2021-03-04 01:42:10 -0700

    Added new Trial class and sub classes

---
+++ app/src/main/java/com/example/trialio/Count.java
@@ -0,0 +1,9 @@
+package com.example.trialio;
+
+public class Count extends Trial{
+    private Integer increment;
+
+    void Count(){
+        increment += 1;
+    }
+}
---
+++ app/src/main/java/com/example/trialio/Trial.java
@@ -0,0 +1,13 @@
+package com.example.trialio;
+
+import android.location.Location;
+
+import java.lang.reflect.Array;
+import java.util.Date;
+
+public class Trial {
+    //protected User experimenter;
+    protected Location location;
+    protected Date date;
+    //protected Image qrcode;
+}
