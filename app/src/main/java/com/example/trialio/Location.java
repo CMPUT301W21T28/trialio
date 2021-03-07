e2e70098bf5d8b62b89390bff48b23d65494d211
ryanjacquess <r.jacques111@gmail.com>
2021-03-07 14:14:17 -0700

    added missing class & empty class

---
+++ app/src/main/java/com/example/trialio/Location.java
@@ -0,0 +1,10 @@
+package com.example.trialio;
+
+import java.util.ArrayList;
+
+public class Location {
+    private double latitude;
+    private double longitude;
+
+    //    public ArrayList<double> getCoord() { }
+}
--- app/src/main/java/com/example/trialio/TrialManager.java
+++ app/src/main/java/com/example/trialio/TrialManager.java
@@ -1,4 +1,19 @@
 package com.example.trialio;
 
+import java.util.Collection;
+
 public class TrialManager {
+    private String type;
+    private Collection<Trial> trials;
+    private Collection<User> ignored;
+    private int minNumOfTrials;
+    private boolean open;
+
+    // public void addNewTrial (Trial trial) { }
+
+    // public void deleteTrial (Trial trial) { }
+
+    // public Collection<Trial> getVisibleTrials() { }
+
+    // public void setExperimenterStatus (User experimenter, boolean ignore) { }
 }
