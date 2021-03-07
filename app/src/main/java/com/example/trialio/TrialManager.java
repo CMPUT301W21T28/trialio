fe06f2911028a117f2a5ec78a9d2a3f18e588975
Quinn Pham <quinnlp12@gmail.com>
2021-03-06 19:37:15 -0700

    feat: big push with experiment manager, and adapter for the main activity

--- app/src/main/java/com/example/trialio/TrialManager.java
+++ app/src/main/java/com/example/trialio/TrialManager.java
@@ -1,4 +1,51 @@
 package com.example.trialio;
 
+import java.util.ArrayList;
+
 public class TrialManager {
+    private String type;
+    private ArrayList<Trial> trials;
+    private ArrayList<User> ignored;
+    private int minNumOfTrials;
+    private Boolean open;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public ArrayList<Trial> getTrials() {
+        return trials;
+    }
+
+    public void setTrials(ArrayList<Trial> trials) {
+        this.trials = trials;
+    }
+
+    public ArrayList<User> getIgnored() {
+        return ignored;
+    }
+
+    public void setIgnored(ArrayList<User> ignored) {
+        this.ignored = ignored;
+    }
+
+    public int getMinNumOfTrials() {
+        return minNumOfTrials;
+    }
+
+    public void setMinNumOfTrials(int minNumOfTrials) {
+        this.minNumOfTrials = minNumOfTrials;
+    }
+
+    public Boolean getOpen() {
+        return open;
+    }
+
+    public void setOpen(Boolean open) {
+        this.open = open;
+    }
 }
--- app/src/main/java/com/example/trialio/User.java
+++ app/src/main/java/com/example/trialio/User.java
@@ -7,5 +7,27 @@ public class User {
     // private Collection<Experiment> subscribedExperiments;
     private BarcodeManager barcodeManager;
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public UserContactInfo getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(UserContactInfo contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public BarcodeManager getBarcodeManager() {
+        return barcodeManager;
+    }
+
+    public void setBarcodeManager(BarcodeManager barcodeManager) {
+        this.barcodeManager = barcodeManager;
+    }
 }
