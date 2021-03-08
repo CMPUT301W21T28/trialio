00171baad226b5f228d7222c963950742252c35a
Mohammed Azad <mmazad@ualberta.ca>
2021-03-08 16:02:06 -0700

    made ExperimentManager implement serializable

--- app/src/main/java/com/example/trialio/ExperimentManager.java
+++ app/src/main/java/com/example/trialio/ExperimentManager.java
@@ -14,9 +14,10 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class ExperimentManager {
+public class ExperimentManager implements Serializable {
     private final String TAG = "ExperimentManager";
 
     private static CollectionReference experimentsCollection = FirebaseFirestore.getInstance().collection("experiments");
