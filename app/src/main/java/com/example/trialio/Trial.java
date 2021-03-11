a23f7e7bb54f37d9f21727359f696e8e979caa0c
Quinn Pham <quinnlp12@gmail.com>
2021-03-11 11:10:51 -0700

    fix: reset Trial so it matches main

--- app/src/main/java/com/example/trialio/Trial.java
+++ app/src/main/java/com/example/trialio/Trial.java
@@ -8,23 +8,40 @@ import java.util.Date;
 
 public class Trial implements Serializable {
     //protected User experimenter;
-    protected Location location;
-    protected Date date;
+    //protected Location location;
+    //protected Date date;
     //protected Image qrcode;
 
-    public Location getLocation() {
+    // Changing these variables to type string for testing adding trials -Jeff
+    protected String location;
+    protected String date;
+    protected String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getLocation() {
         return location;
     }
 
-    public void setLocation(Location location) {
+    public void setLocation(String location) {
         this.location = location;
     }
 
-    public Date getDate() {
+    public String getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(String date) {
         this.date = date;
     }
+
+
+
+
 }
