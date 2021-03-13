90473d30d07d12255d83144827b2d35cb97ba624
Quinn Pham <quinnlp12@gmail.com>
2021-03-13 16:37:05 -0700

    fix: #43 Location now implements Serializable

--- app/src/main/java/com/example/trialio/models/Location.java
+++ app/src/main/java/com/example/trialio/models/Location.java
@@ -1,6 +1,8 @@
 package com.example.trialio.models;
 
-public class Location {
+import java.io.Serializable;
+
+public class Location implements Serializable {
     private double latitude;
     private double longitude;
 
