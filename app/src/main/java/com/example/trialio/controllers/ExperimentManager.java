eda959c2ef05900e5f6c06dc4e0e0e8c123cf1de
Diego Becerra <diego.enrique.becerra@gmail.com>
2021-03-13 22:41:34 -0700

    add callback listeners for fetching experiments

--- app/src/main/java/com/example/trialio/controllers/ExperimentManager.java
+++ app/src/main/java/com/example/trialio/controllers/ExperimentManager.java
@@ -13,9 +13,12 @@ import com.example.trialio.models.NonNegativeTrial;
 import com.example.trialio.models.Location;
 import com.example.trialio.models.Trial;
 import com.example.trialio.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -33,6 +36,36 @@ public class ExperimentManager {
     private ArrayList<Experiment> experimentList;
     private ArrayAdapter<Experiment> experimentAdapter;
 
+    /**
+     * This interface represents an action to be taken when an Experiment document is fetched from
+     * the database.
+     */
+    public interface OnExperimentFetchListener {
+
+        /**
+         * This method will be called when an Experiment is fetched from the database.
+         *
+         * @param experiment the experiment that was fetched from the database
+         */
+        public void onExperimentFetch(Experiment experiment);
+    }
+
+    /**
+     * This interface represents an action to be taken when a collection of Experiments is fetched
+     * from the database.
+     */
+    public interface OnManyExperimentsFetchListener {
+        /**
+         * This methid will be called when a collection of Experiments is fetched from the database.
+         *
+         * @param experiments the experiments that were fetched from the database
+         */
+        public void onManyExperimentsFetch(ArrayList<Experiment> experiments);
+    }
+
+    /**
+     * Constructor for ExperimentManager
+     */
     public ExperimentManager() {
         experimentList = new ArrayList<Experiment>();
 
@@ -56,7 +89,7 @@ public class ExperimentManager {
                         for (Map trial : (ArrayList<Map>) tm.get("trials")) {
                             Map location = (Map) trial.get("location");
 
-                            experiment.getTrialManager().addTrial(new BinomialTrial((String)trial.get("experimenterId"), new Location((double)location.get("latitude"), (double)location.get("longitude")),((com.google.firebase.Timestamp)trial.get("date")).toDate(), (boolean)trial.get("isSuccess")));
+                            experiment.getTrialManager().addTrial(new BinomialTrial((String) trial.get("experimenterId"), new Location((double) location.get("latitude"), (double) location.get("longitude")), ((com.google.firebase.Timestamp) trial.get("date")).toDate(), (boolean) trial.get("isSuccess")));
                         }
                     } else if (ExperimentTypeUtility.isCount(tm.get("type").toString())) {
                         // TODO
@@ -64,7 +97,7 @@ public class ExperimentManager {
                         for (Map trial : (ArrayList<Map>) tm.get("trials")) {
                             Map location = (Map) trial.get("location");
 
-                            experiment.getTrialManager().addTrial(new NonNegativeTrial((String)trial.get("experimenterId"), new Location((double)location.get("latitude"), (double)location.get("longitude")),((com.google.firebase.Timestamp)trial.get("date")).toDate(), ((java.lang.Long)trial.get("nonNegCount")).intValue()));
+                            experiment.getTrialManager().addTrial(new NonNegativeTrial((String) trial.get("experimenterId"), new Location((double) location.get("latitude"), (double) location.get("longitude")), ((com.google.firebase.Timestamp) trial.get("date")).toDate(), ((java.lang.Long) trial.get("nonNegCount")).intValue()));
                         }
                     } else if (ExperimentTypeUtility.isMeasurement(tm.get("type").toString())) {
                         // TODO
@@ -85,9 +118,12 @@ public class ExperimentManager {
     }
 
     /**
+     * DO NOT USE THIS
+     * Use setOnExperimentListFetchListener and set the adapter manually instead.
+     * -------------------------------------------------------------------------
      * This sets an adapter for the experiment manager
-     * @param adapter
-     * Candidate adapter to set
+     *
+     * @param adapter Candidate adapter to set
      */
     public void setAdapter(ArrayAdapter adapter) {
         experimentAdapter = adapter;
@@ -95,36 +131,100 @@ public class ExperimentManager {
 
     /**
      * This adds an experiment to the database
-     * @param experiment
-     * Candidate experiment to add to the database
+     *
+     * @param experiment Candidate experiment to add to the database
      */
-    public void publishExperiment (Experiment experiment) {
-        Log.d(TAG, "Adding " + experiment.toString());
+    public void publishExperiment(Experiment experiment) {
+        Log.d(TAG, "Adding experiment" + experiment.toString());
+        String id = experiment.getExperimentID();
         experimentsCollection
-                .document(experiment.getExperimentID())
+                .document(id)
                 .set(experiment)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        Log.d(TAG, "Data was added!");
+                        String message = "Experiment " + id + " was successfully added.";
+                        Log.d(TAG, message);
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.d(TAG, "Data was not added!" + e.toString());
+                        String message = "Failed to add experiment " + id;
+                        Log.d(TAG, message);
+                    }
+                });
+    }
+
+    /**
+     * Sets a function to be called when an experiment is fetched
+     *
+     * @param experimentId the id of the experiment to fetch
+     * @param listener     the function to be called when the experiment is fetched
+     */
+    public void setOnExperimentFetchCallback(String experimentId, OnExperimentFetchListener listener) {
+        // https://firebase.google.com/docs/firestore/query-data/get-data
+        Log.d(TAG, "Fetching experiment " + experimentId);
+        DocumentReference docRef = experimentsCollection.document(experimentId);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot doc = task.getResult();
+                    if (doc.exists()) {
+                        Experiment experiment = extractExperimentDocument(doc);
+                        listener.onExperimentFetch(experiment);
+                        Log.d(TAG, "Experiment " + experimentId + " fetched successfully.");
+                    } else {
+                        Log.d(TAG, "No experiment found with id " + experimentId);
+                    }
+                } else {
+                    Log.d(TAG, "Experiment fetch failed with " + task.getException());
+                }
+            }
+        });
+    }
+
+
+    /**
+     * Sets a function to be called when all experiment are fetched
+     *
+     * @param listener the function to be called when the experiments are fetched
+     */
+    public void setOnAllExperimentsFetchCallback(OnManyExperimentsFetchListener listener) {
+        // https://firebase.google.com/docs/firestore/query-data/get-data#get_all_documents_in_a_collection
+        Log.d(TAG, "Fetching all experiment");
+        experimentsCollection
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            String message = "All experiments fetched successfully";
+                            Log.d(TAG, message);
+
+                            QuerySnapshot qs = task.getResult();
+                            ArrayList<Experiment> experimentList = new ArrayList<>();
+                            for (DocumentSnapshot doc : qs.getDocuments()) {
+                                Experiment experiment = extractExperimentDocument(doc);
+                                experimentList.add(experiment);
+                            }
+                            listener.onManyExperimentsFetch(experimentList);
+                        } else {
+                            String message = "Failed to fetch all experiments";
+                            Log.d(TAG, message);
+                        }
                     }
                 });
     }
 
     /**
      * This sets the experiment with a given experiment ID as a given edited experiment
-     * @param experimentId
-     * Experiment ID of candidate experiment to edit
-     * @param experiment
-     * Candidate edited experiment to set
+     *
+     * @param experimentId Experiment ID of candidate experiment to edit
+     * @param experiment   Candidate edited experiment to set
      */
-    public void editExperiment (String experimentId, Experiment experiment) {
+    public void editExperiment(String experimentId, Experiment experiment) {
         Log.d(TAG, "Editing " + experimentId + "with" + experiment.toString());
         experimentsCollection
                 .document(experimentId)
@@ -145,10 +245,10 @@ public class ExperimentManager {
 
     /**
      * This deletes an experiment associated with a given experiment ID
-     * @param experimentId
-     * Experiment ID of the candidate experiment to delete
+     *
+     * @param experimentId Experiment ID of the candidate experiment to delete
      */
-    public void unpublishExperiment (String experimentId) {
+    public void unpublishExperiment(String experimentId) {
         Log.d(TAG, "Deleting " + experimentId);
         experimentsCollection
                 .document(experimentId)
@@ -169,12 +269,11 @@ public class ExperimentManager {
 
     /**
      * This finds the list of all experiments owned by a given user
-     * @param owner
-     * User for which to find all of their owned experiments
-     * @return
-     * Returns the list of experiments owned by owner
+     *
+     * @param owner User for which to find all of their owned experiments
+     * @return Returns the list of experiments owned by owner
      */
-    public ArrayList<Experiment> getOwnedExperiments (User owner) {
+    public ArrayList<Experiment> getOwnedExperiments(User owner) {
         // TODO: test
         ArrayList<Experiment> ownedExperiments = new ArrayList<Experiment>();
         for (Experiment experiment : experimentList) {
@@ -187,12 +286,11 @@ public class ExperimentManager {
 
     /**
      * This finds the list of experiments associated with a given keyword
-     * @param keyword
-     * String keyword to search for
-     * @return
-     * Returns the list of experiments associated with keyword
+     *
+     * @param keyword String keyword to search for
+     * @return Returns the list of experiments associated with keyword
      */
-    public ArrayList<Experiment> searchByKeyword (String keyword) {
+    public ArrayList<Experiment> searchByKeyword(String keyword) {
         // TODO: test
         ArrayList<Experiment> searchResults = new ArrayList<Experiment>();
         for (Experiment experiment : experimentList) {
@@ -204,9 +302,10 @@ public class ExperimentManager {
     }
 
     /**
+     * DO NOT USE THIS
      * This returns the current experiment list
-     * @return
-     * Returns the list of experiments
+     *
+     * @return Returns the list of experiments
      */
     public ArrayList<Experiment> getExperimentList() {
         return experimentList;
@@ -214,11 +313,50 @@ public class ExperimentManager {
 
     /**
      * This generates a new unique experiment ID
-     * @return
-     * Returns a string which is a new experiment ID
+     *
+     * @return Returns a string which is a new experiment ID
      */
     public static String getNewExperimentID() {
         return experimentsCollection.document().getId();
+    }
+
+    /**
+     * Extracts an experiment object from a Firestore document. This method assumes the document
+     * hold a valid Experiment.
+     *
+     * @param document the document to be extracted
+     * @return the extracted experiment
+     */
+    private Experiment extractExperimentDocument(DocumentSnapshot document) {
+        Experiment experiment = document.toObject(Experiment.class);
+
+        // clear the trials since they do not have subclass specific attributes
+        experiment.getTrialManager().setTrials(new ArrayList<Trial>());
+
+        Map data = document.getData();
+        Map tm = (Map) data.get("trialManager");
+
+        // cast trials to the appropriate type and add them to the trial manager
+        if (ExperimentTypeUtility.isBinomial(tm.get("type").toString())) {
+            for (Map trial : (ArrayList<Map>) tm.get("trials")) {
+                Map location = (Map) trial.get("location");
+
+                experiment.getTrialManager().addTrial(new BinomialTrial((String) trial.get("experimenterId"), new Location((double) location.get("latitude"), (double) location.get("longitude")), ((com.google.firebase.Timestamp) trial.get("date")).toDate(), (boolean) trial.get("isSuccess")));
+            }
+        } else if (ExperimentTypeUtility.isCount(tm.get("type").toString())) {
+            // TODO
+        } else if (ExperimentTypeUtility.isNonNegative(tm.get("type").toString())) {
+            for (Map trial : (ArrayList<Map>) tm.get("trials")) {
+                Map location = (Map) trial.get("location");
+
+                experiment.getTrialManager().addTrial(new NonNegativeTrial((String) trial.get("experimenterId"), new Location((double) location.get("latitude"), (double) location.get("longitude")), ((com.google.firebase.Timestamp) trial.get("date")).toDate(), ((java.lang.Long) trial.get("nonNegCount")).intValue()));
+            }
+        } else if (ExperimentTypeUtility.isMeasurement(tm.get("type").toString())) {
+            // TODO
+        } else {
+            assert (false);
+        }
+        return experiment;
     }
 
 }
