1b1f37ff13725220776ecd544f0830e1344b6995
yuchieh8968 <yuchieh@ualberta.ca>
2021-03-17 14:48:37 -0600

    have current user info set has preset for edittext in fragment

--- app/src/main/java/com/example/trialio/fragments/EditProfileFragment.java
+++ app/src/main/java/com/example/trialio/fragments/EditProfileFragment.java
@@ -14,37 +14,49 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.trialio.R;
+import com.example.trialio.controllers.UserManager;
+import com.example.trialio.models.Experiment;
+import com.example.trialio.models.User;
+import com.example.trialio.utils.ExperimentTypeUtility;
+
+import java.io.Serializable;
 
 public class EditProfileFragment extends DialogFragment {
     private EditText username;
     private EditText phoneNumber;
     private EditText email;
     private OnFragmentInteractionListener listener;
-
+    private User user;
+    private Integer integer;
+    private Bundle bundle;
     public interface OnFragmentInteractionListener {
-        //void onConfirm(); change user info
+        void onConfirm();
     }
 
-    //if some user info exists, send it to the dialog box as a default value
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener){
-            listener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         View view = LayoutInflater.from(getActivity()).inflate(R.layout.edit_profile_fragment_layout, null);
+
+        // get the experiment that was passed in
+        Bundle bundle = getArguments();
+        user = (User) bundle.getSerializable("UserProfile");
+
         username = view.findViewById(R.id.userNameText);
         phoneNumber = view.findViewById(R.id.editUserPhone);
         email = view.findViewById(R.id.editUserEmail);
+
+        UserManager manager = new UserManager();
+        manager.addCurrentUserUpdateListener(new UserManager.OnUserFetchListener() {
+            @Override
+            public void onUserFetch(User user) {
+                username.setText(user.getUsername());
+                phoneNumber.setText(user.getContactInfo().getPhone());
+                email.setText(user.getContactInfo().getEmail());
+            }
+        });
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         return builder
