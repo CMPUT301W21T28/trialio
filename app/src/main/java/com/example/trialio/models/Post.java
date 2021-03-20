6e4c38454794e9b46e5fa7358ca6f7df2f2b4a85
ryanjacquess <r.jacques111@gmail.com>
2021-03-19 23:36:25 -0600

    models docs

--- app/src/main/java/com/example/trialio/models/Post.java
+++ app/src/main/java/com/example/trialio/models/Post.java
@@ -2,11 +2,19 @@ package com.example.trialio.models;
 
 import com.example.trialio.models.User;
 
+/**
+ * Represents a post on an experiment's question forum
+ */
 public abstract class Post {
 
     private String body;
     private User user;
 
+    /**
+     * Gets the body of a post
+     *
+     * @return the body of a post
+     */
     public String getBody() {
         return this.body;
     }
