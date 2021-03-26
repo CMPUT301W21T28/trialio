752fe5596eb47deb04b52bf00c86ba83f871d02a
Diego Becerra <diego.enrique.becerra@gmail.com>
2021-03-25 23:44:25 -0600

    fix some merge shenanaigans

--- app/src/main/java/com/example/trialio/activities/ViewUserActivity.java
+++ app/src/main/java/com/example/trialio/activities/ViewUserActivity.java
@@ -9,9 +9,9 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.trialio.R;
+import com.example.trialio.fragments.EditContactInfoFragment;
 import com.example.trialio.models.User;
 import com.example.trialio.controllers.UserManager;
-import com.example.trialio.fragments.EditContactInfoFragment;
 
 // Code referenced from Stack Overflow thread Android custom back button with text https://stackoverflow.com/questions/46242280/android-custom-back-button-with-text
 // by user Nuovo 001, profile https://stackoverflow.com/users/8615244/nuovo-001
@@ -20,9 +20,12 @@ import com.example.trialio.fragments.EditContactInfoFragment;
 /**
  * This activity allows a user to view their own profile and edit it to make changes to their username and contract info
  */
-
 public class ViewUserActivity extends AppCompatActivity {
+    private final String TAG = "ViewUserActivity";
+
     private User user;
+    private Button editUserProfile;
+    private UserManager userManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,30 +95,48 @@ public class ViewUserActivity extends AppCompatActivity {
         TextView userPhone = findViewById(R.id.phoneText);
         TextView UserEmail = findViewById(R.id.emailText);
 
-        UserManager manager = new UserManager();
-        manager.getCurrentUser(new UserManager.OnUserFetchListener() {
+        // set text views
+        userName.setText(user.getUsername());
+        userPhone.setText(user.getContactInfo().getPhone());
+        UserEmail.setText(user.getContactInfo().getEmail());
+    }
+
+    /**
+     * This sets the visibility of the edit button in the ViewUserActivity.
+     */
+    public void setVisibility() {
+
+        // by default the editUserProfile button is invisible
+        editUserProfile.setVisibility(View.INVISIBLE);
+
+        // get current user
+        userManager.getCurrentUser(new UserManager.OnUserFetchListener() {
             @Override
-            public void onUserFetch(User user) {
-                manager.addUserUpdateListener(user.getUsername(), new UserManager.OnUserFetchListener() {
-                    @Override
-                    public void onUserFetch(User user) {
-                        userName.setText(user.getUsername());
-                        userPhone.setText(user.getContactInfo().getPhone());
-                        UserEMail.setText(user.getContactInfo().getEmail());
-                    }
-                });
+            public void onUserFetch(User currentUser) {
+                // compare current user with arg user. If same id, make the edit button visible
+                if (user.getUsername().equals(currentUser.getUsername())) {
+                    editUserProfile.setVisibility(View.VISIBLE);
+                }
             }
         });
+    }
+
+    /**
+     * This sets the onclick listeners of the buttons in ViewUserActivity.
+     */
+    public void setOnClickListeners() {
 
         // show a fragment to edit user profile
         editUserProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditContactInfoFragment editContactInfoFragment = new EditContactInfoFragment();
+                EditContactInfoFragment editProfileFragment = new EditContactInfoFragment();
                 Bundle args = new Bundle();
-                args.putSerializable("UserProfile", (Serializable) user);
-                editContactInfoFragment.setArguments(args);
-                editContactInfoFragment.show(getSupportFragmentManager(), "editProfile");
+
+                args.putSerializable("UserProfile", user);
+                editProfileFragment.setArguments(args);
+
+                editProfileFragment.show(getSupportFragmentManager(), "editProfile");
             }
         });
     }
