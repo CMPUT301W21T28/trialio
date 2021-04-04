a54bd081d7d6877d60e7a4f6d0991cc37daaef27
Mohammed Azad <mmazad@ualberta.ca>
2021-04-04 17:51:41 -0600

    removal of unnecessary code

--- app/src/main/java/com/example/trialio/activities/ExperimentCreateActivity.java
+++ app/src/main/java/com/example/trialio/activities/ExperimentCreateActivity.java
@@ -201,32 +201,10 @@ public class ExperimentCreateActivity extends AppCompatActivity implements OnMap
         // set the home button
         HomeButtonUtility.setHomeButtonListener(findViewById(R.id.button_home));
     }
-
-    public static boolean isConnected(Context context) {
-            ConnectivityManager connectivityManager = (ConnectivityManager)
-                    context.getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo networkInfo = null;
-            if (connectivityManager != null) {
-                networkInfo = connectivityManager.getActiveNetworkInfo();
-            }
-
-            return networkInfo != null && networkInfo.getState() == NetworkInfo.State.CONNECTED;
-    }
-
-
+    
 
     private String findRegionName(Location location) {
-        if (isConnected(ExperimentCreateActivity.this)) {
-            //carry on with function
-        } else {
-            try {
-                wait(60);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
         Log.d(TAG, "Getting Location Name");
-
         //working on running the geocoder in a background thread
         Geocoder geocoder = new Geocoder(ExperimentCreateActivity.this);
         String regionName = null;
