03f3299654c3beea29e339863cd187e100af4d2c
Diego Becerra <diego.enrique.becerra@gmail.com>
2021-04-04 11:47:39 -0600

    add stubs to test funcs

--- app/src/androidTest/java/com/example/trialio/ExperimentManagerTest.java
+++ app/src/androidTest/java/com/example/trialio/ExperimentManagerTest.java
@@ -1,4 +1,61 @@
 package com.example.trialio;
 
+import com.example.trialio.controllers.ExperimentManager;
+import com.example.trialio.models.Experiment;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
 public class ExperimentManagerTest {
+
+    @Before
+    public void startUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    private ExperimentManager mockExperimentManager() {
+        return null;
+    }
+
+    private ExperimentManager mockEmptyExperimentManager() {
+        return null;
+    }
+
+    @Test
+    public void testPublishExperiment() {
+        fail();
+    }
+
+    @Test
+    public void testSetOnExperimentFetchListener() {
+        fail();
+    }
+
+    @Test
+    public void testEditExperiment() {
+        fail();
+    }
+
+    @Test
+    public void testUnpublishExperiment() {
+        fail();
+    }
+
+    @Test
+    public void testGetOwnedExperiments() {
+        fail();
+    }
+
+    @Test
+    public void testSearchByKeyword() {
+        fail();
+    }
 }
