c43602917246f0b46e62bc47ddc45f9e81eb7abd
Diego Becerra <diego.enrique.becerra@gmail.com>
2021-04-08 22:54:51 -0600

    add comment

--- app/src/main/java/com/example/trialio/activities/ExperimentActivity.java
+++ app/src/main/java/com/example/trialio/activities/ExperimentActivity.java
@@ -100,7 +100,8 @@ public class ExperimentActivity extends AppCompatActivity implements NonNegative
         // get the experiment that was passed in as an argument
         Bundle bundle = getIntent().getExtras();
         experiment = (Experiment) bundle.getSerializable("experiment");
-        currentUser = (User) bundle.getSerializable("user_exp");
+
+        // get the current user
         CurrentUserHandler.getInstance().getCurrentUser(new CurrentUserHandler.OnUserFetchCallback() {
             @Override
             public void onUserFetch(User user) {
